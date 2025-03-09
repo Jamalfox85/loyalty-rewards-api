edd5430f1c52a0115cbbad735d4be733ec420284
Jamal Fox <jamalfox@Mac.attlocal.net>
2025-03-08 23:49:12 -0500

    render commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # loyalty-rewards-api
+
+## start up
+
+```
+    go run main.go
+
+    ngrok http --url=supposedly-simple-hamster.ngrok-free.app 8080 #Preferred
+    or
+    ngrok http 8080
+```
+
+## ngrok
+
+Connecting to BigCommerce APIs require a public facing api. By utilizing ngrok we can expose this api publicly, allowing us to hit BC routes
+
+```
+    ngrok http --url=supposedly-simple-hamster.ngrok-free.app 8080 #Preferred
+    or
+    ngrok http 8080
+```
+
+exposts terminal at localhost:8080 - adjust to match host if needed
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,7 @@ func main() {
 
 	// Enable CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Allow only your frontend origin
+		AllowOrigins:     []string{"https://api.bigcommerce.com"}, // Allow only your frontend origin
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
@@ -22,8 +23,9 @@ func main() {
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "CORS is working!"})
 	})
-	r.GET("/oauth", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "CORS is working!"})
+	r.GET("/auth", func(c *gin.Context) {
+		// c.JSON(http.StatusOK, gin.H{"message": "CORS is working!"})
+		c.Redirect(http.StatusFound, "https://supposedly-simple-hamster.ngrok-free.app")
 	})
 	r.GET("/load", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "CORS is working!"})
@@ -35,5 +37,9 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"message": "CORS is working!"})
 	})
 
-	r.Run(":8080") // Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port if not set
+	}
+	r.Run(":" + port) // Start server
 }
